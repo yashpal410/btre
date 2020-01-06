a0e512c0bb82228ab280064c14a279a386c74964
yashpal410 <59008150+yashpal410@users.noreply.github.com>
2020-01-07 00:32:47 +0530

    15

--- try.py
+++ try.py
@@ -8,12 +8,6 @@ root= tk.Tk()
 canvas1 = tk.Canvas(root, width = 300, height = 300, bg = 'lightsteelblue')
 canvas1.pack()
 
-# app = Flask(__name__)
-#
-# @app.route('/')
-# def index():
-#     return "{}".format(df)
-
 
 
 def getExcel ():
@@ -21,14 +15,11 @@ def getExcel ():
 
     import_file_path = filedialog.askopenfilename()
     df = pd.read_excel (import_file_path)
-    open("link.html", "w").write("{}".format(df))
+    open("link.html", "w").write("{}".format(df.to_html())
     webbrowser.open("link.html")
-    #app.run()
-    #return df
 
 def getFile():
     getExcel()
-    #print(df)
 
 browseButton_Excel = tk.Button(text='Import Excel File', command=getFile, bg='green', fg='white', font=('helvetica', 12, 'bold'))
 canvas1.create_window(150, 150, window=browseButton_Excel)
