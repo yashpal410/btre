ea40f222910133691177bb658a97a8f2da6d66f5
yashpal410 <59008150+yashpal410@users.noreply.github.com>
2020-01-07 22:56:27 +0530

    update

--- tool.py
+++ tool.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, send_file
 from werkzeug import secure_filename
+import pandas as pd
 
 app = Flask(__name__)
 
@@ -12,11 +13,8 @@ def success():
     global file
     if request.method=='POST':
         file = request.files["file"]
-        file.save(secure_filename("uploaded" + file.filename))
-        print(file)
-        print(type(file))
-        return render_template("index.html", text = df.to_html())
+        df = pd.read_excel(file)
+        return render_template("index.html",text=df.to_html())
 
 if __name__ == '__main__':
     app.run()
-
